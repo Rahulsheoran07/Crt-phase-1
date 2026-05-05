29ade52715f33eda7ec127b0c2f5f9f27879d30d
rahulsheoran07 <rahulsheoran346d@gmail.com>
2026-05-05 15:48:26 +0530

    commit all changes

---
+++ Day5/Function/discount_reference.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void applyDiscount(double &billAmount) {
+    const double discountRate = 0.10; // 10%
+    billAmount -= billAmount * discountRate;
+}
+
+int main() {
+    double billAmount;
+    cout << "Enter original bill amount: ";
+    cin >> billAmount;
+
+    applyDiscount(billAmount);
+
+    cout << "Bill amount after 10% discount: " << billAmount << endl;
+    return 0;
+}
