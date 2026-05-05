0ea8ee9e631a94875d68cae869149573f21ee268
rahulsheoran07 <rahulsheoran346d@gmail.com>
2026-05-05 13:33:29 +0530

    commit all changes

---
+++ Day5/basic_function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void printMsg()
+{
+    cout << "Hello jecrc From Function" << endl;
+}
+int main()
+{
+    cout << "Hello World!" << endl;
+    printMsg();
+    return 0;
+}
